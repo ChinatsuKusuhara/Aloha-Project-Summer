66593949a3b2e01f66d0fabcd67f90cb9e53f3ef
Ayesha Kanani <isha@Ayesha-Kananis-MacBook-Pro.local>
2017-07-14 15:50:50 -0700

    added flckity

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(function() {
-  
+  $('.main-carousel').flickity({
+  cellAlign: 'center',
+  contain: true,
+  autoPlay:1000,
+  percentPosition: false
+});
+  console.log("this isn't working");
 });
