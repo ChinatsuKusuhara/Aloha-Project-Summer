09f61675877e187c77a51834cd35c92906412c59
Ayesha Kanani <isha@Ayesha-Kananis-MacBook-Pro.local>
2017-07-17 14:37:47 -0700

    changed carosell and sign up form

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ $(function() {
   $('.main-carousel').flickity({
   cellAlign: 'center',
   contain: true,
-  autoPlay:1000,
-  percentPosition: false
+  autoPlay:3000,
+  percentPosition: false,
+  setGallerySize:true
 });
   console.log("this isn't working");
 });
